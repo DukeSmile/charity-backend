545287e3a1ee1f6f3cbf31e5de67e79f542175c1
DukeSmile <mitchelldev232@gmail.com>
2022-11-29 19:25:01 +0900

    fix: auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from '../users/user.module';
 
 @Module({
   imports: [UsersModule],
